bb27b91badc9d4a9c54fc72baefb7f00abd8eb98
yoonsun124 <dbstjs0124@gmail.com>
2024-08-14 22:14:39 +0900

    first commit

---
+++ src/main/java/com/restapi/emp/BackEndRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.emp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndRestApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/restapi/emp/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.restapi.emp.repository;
+
+import com.restapi.emp.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByEmail(String email);
+    List<Employee> findByFirstNameContains(String email);
+}
